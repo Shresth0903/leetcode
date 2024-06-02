70575f0d2b8a6e461a3880238ab9cf84e5d5cc5e
Shresth Pandey <94431035+Shresth0903@users.noreply.github.com>
2024-06-02 22:25:49 +0530

    Time: 0 ms (100%), Space: 49.3 MB (17.84%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,14 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        int i = 0;
-        int j = s.length - 1;
-        solve(s, i, j);
-    }
-    public void solve(char[] s, int i, int j){
-        if(i >= j) return;
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        solve(s, ++i, --j);
+        int start = 0;
+        int end = s.length - 1;
+        while(start < end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
